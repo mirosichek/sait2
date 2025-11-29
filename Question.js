f6e28f1078c2237b4318b74ee11bd653cce9e138
mirosick <mirosickimya@gmail.com>
2025-11-29 10:14:53 +0300

    Inset question

--- Question.js
+++ Question.js
@@ -1,37 +1,52 @@
+
+
 const supabaseUrl = 'https://zxdprwvrgzqpxyvxksud.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp4ZHByd3ZyZ3pxcHh5dnhrc3VkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM1MDI5NjQsImV4cCI6MjA3OTA3ODk2NH0.Rd8UXhHz97ifIrSezDkcBNzBn18_wAVtY_mUVCf32FE';
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
-class Question {
-    constructor() {
-        this.question = "";
-        this.answer = "";
+
+class AbstractInput {
+    constructor(Id) {
+        if (new.target === AbstractInput) {
+            throw new Error("Нельзя создавать экземпляры абстрактного класса");
+        }
+        this.Id = Id;
+        this.input = "";
     }
 
     update() {
-        this.question = document.getElementById("myQuestion").value;
-        this.answer = document.getElementById("Answer").value;
+        this.input = document.getElementById(this.Id).value;
 
-        this.render();
-        this.log();
+        this.toDatabase();
+        this.save();
     }
 
-    render() {
-        document.getElementById("result").innerText =
-            "Вопрос: " + this.question + "\nОтвет: " + this.answer;
+    toDatabase() {
+        throw new Error("Метод toDatabase() должен быть реализован в наследнике");
     }
 
-    log() {
-        console.log("Сохранено:", {
-            question: this.question,
-            answer: this.answer
-        });
+    save() {
+        throw new Error("Метод save() должен быть реализован в наследнике");
+    }
+}
+
+
+class Question extends AbstractInput {
+    constructor() {
+        super("myQuestion");
+    }
+
+    async toDatabase() {
+        const { error } = await supabase
+            .from('Questions')
+            .insert({ name: this.input });
+
+        if (error) console.error(error);
+    }
+
+    save() {
+        console.log("Сохранено:", this.input);
     }
 }
 
 const savedValue = new Question();
-
-function save() {
-    savedValue.update();
-}
-
