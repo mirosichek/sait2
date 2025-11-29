ed2c2ea5956bb499f585ec2a754cfb6b3f2c7cf1
mirosick <mirosickimya@gmail.com>
2025-11-29 11:05:15 +0300

    Inset question

--- Question.js
+++ Question.js
@@ -39,7 +39,7 @@ class Question extends AbstractInput {
     async toDatabase() {
         const { error } = await supabase
             .from('Questions')
-            .insert({id:1, Question: this.input });
+            .insert({id:2, Question: this.input });
 
         if (error) console.error(error);
     }
