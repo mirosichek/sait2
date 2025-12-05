5d7c2b69aca49b18dfda816aa82daee1a85d1ebf
mirosick <mirosickimya@gmail.com>
2025-12-05 11:23:14 +0300

    Question class

--- Question.js
+++ Question.js
@@ -2,7 +2,7 @@
 
 const supabaseUrl = 'https://zxdprwvrgzqpxyvxksud.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp4ZHByd3ZyZ3pxcHh5dnhrc3VkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM1MDI5NjQsImV4cCI6MjA3OTA3ODk2NH0.Rd8UXhHz97ifIrSezDkcBNzBn18_wAVtY_mUVCf32FE';
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
 
 class Question {
