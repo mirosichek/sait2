fd8bceee08dcdc78d1f1ccee918e8eafbc3f5f56
mirosick <mirosickimya@gmail.com>
2025-12-05 11:14:04 +0300

    Question class

--- Question.js
+++ Question.js
@@ -5,35 +5,6 @@ const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
 
-// class AbstractInput {
-//     constructor(elementId) {
-//         if (new.target === AbstractInput) {
-//             throw new Error("Нельзя создавать экземпляры абстрактного класса");
-//         }
-//         this.elementId = elementId;
-//         this.element = document.getElementById(elementId);
-//         this.input = "";
-//         this.setupEventListener();
-//     }
-
-//     setupEventListener() {
-//         this.element.addEventListener('keydown', (event) => {
-//             if (event.key === 'Enter') {
-//                 const value = event.target.value;
-//                 this.input = value;
-//                 this.toDatabase(value);
-//             }
-//         });
-//     }
-//     update() {
-//         this.element.value = "";
-//     }
-
-//     toDatabase() {
-//         throw new Error("Метод toDatabase() должен быть реализован в наследнике");
-//     }
-// }
-
 class Question {
     constructor(elementId) {
         this.element = document.getElementById(elementId);
