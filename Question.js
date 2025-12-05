27e26b6168173340566a69f000ddf22f1b198081
mirosick <mirosickimya@gmail.com>
2025-12-05 09:41:05 +0300

    Answers add input

--- Question.js
+++ Question.js
@@ -22,7 +22,6 @@ class AbstractInput {
                 const value = event.target.value;
                 this.input = value;
                 this.toDatabase(value);
-                this.update();
             }
         });
     }
@@ -33,7 +32,6 @@ class AbstractInput {
     toDatabase() {
         throw new Error("Метод toDatabase() должен быть реализован в наследнике");
     }
-
 }
 
 class Question extends AbstractInput {
@@ -48,6 +46,38 @@ class Question extends AbstractInput {
         if (error) console.error(error);
         else console.log("Вопрос сохранен в БД:", question);
     }
+}
+
+class Answer extends AbstractInput {
+     constructor() {
+    super("Answer");
+     }
+    async addInputs() {
+  let numberOfInputs = parseInt(document.getElementById('inputCount').value);
+  if (isNaN(numberOfInputs) || numberOfInputs <= 0 || numberOfInputs > 10) {
+    alert('Введите корректное число от 1 до 10. В противном случае что-то может пойти не так!');
+    return;
+  }
+
+  let container = document.getElementById('inputContainer'); // Это место для новых полей
+  while (container.hasChildNodes()) { 
+    container.removeChild(container.lastChild); // Освобождаем место от старых элементов
+  }
+
+  for (let i = 0; i < numberOfInputs; i++) {
+    let input = document.createElement('input');
+    input.type = 'text';
+    input.name = `dynamicInput${i}`; // Имена для каждого поля для избежания ошибок
+    input.placeholder = `Ввод ${i + 1}`;
+    container.appendChild(input); // Добавляем новое поле в контейнер
+    container.appendChild(document.createElement('br')); // Предусматриваем дополнительное пространство между полями
+  }
+}
+
+
+
+
+     
 }
 
 const savedValue = new Question();
